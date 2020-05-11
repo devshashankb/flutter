6b77ff6107e4c88a509ee5e9ea3a0f1b9047f87c
shashank <shashank292@gmail.com>
2020-05-11 09:49:53 -0500

    Added tests for refresh weather

---
+++ test/blocs/weather_bloc_test.dart
@@ -0,0 +1,44 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:weatherapplication/blocs/blocs.dart';
+import 'package:weatherapplication/models/models.dart';
+import 'package:weatherapplication/repositories/repositories.dart';
+
+class MockWeatherBloc extends MockBloc<WeatherEvent, WeatherState>
+    implements WeatherBloc {}
+
+class MockWeatherRepository extends Mock implements WeatherRepository {}
+
+void main() {
+  MockWeatherRepository mockWeatherRepository;
+  Weather weather = Weather(
+      minTemp: 0.0,
+      maxTemp: 10.0,
+      temp: 9.0,
+      formattedCondition: 'Light Cloud',
+      locationId: 2379574,
+      location: 'Chicago',
+      lastUpdated: DateTime.now());
+
+  setUp(() {
+    mockWeatherRepository = MockWeatherRepository();
+  });
+
+  test('emits [WeatherLoading, WeatherLoaded] when successful', () async {
+    when(mockWeatherRepository.getWeather(any))
+        .thenAnswer((_) async => weather);
+    final bloc = WeatherBloc(weatherRepository: mockWeatherRepository);
+    bloc.add(FetchWeather(city: 'Chicago'));
+    await emitsExactly(
+        bloc, [WeatherLoading(), WeatherLoaded(weather: weather)]);
+  });
+
+  test('emits [WeatherLoaded] when successful', () async {
+    when(mockWeatherRepository.getWeather(any))
+        .thenAnswer((_) async => weather);
+    final bloc = WeatherBloc(weatherRepository: mockWeatherRepository);
+    bloc.add(RefreshWeather(city: 'Chicago'));
+    await emitsExactly(bloc, [WeatherLoaded(weather: weather)]);
+  });
+}
